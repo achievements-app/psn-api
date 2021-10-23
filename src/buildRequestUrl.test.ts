3559a2035468d7e16cd8e7a076782148bae27544
Wes Copeland <wlcopeland1@gmail.com>
2021-10-23 14:48:39 -0400

    test(buildRequestUrl): add coverage

---
+++ src/buildRequestUrl.test.ts
@@ -0,0 +1,31 @@
+import { buildRequestUrl } from "./buildRequestUrl";
+
+describe("Util: buildRequestUrl", () => {
+  it("is defined #sanity", () => {
+    // ASSERT
+    expect(buildRequestUrl).toBeDefined();
+  });
+
+  it("returns a URL correctly", () => {
+    // ARRANGE
+    const baseUrl = "https://x.y.achievements.app/api/v1/";
+    const endpointUrl = "/foo/bar/:baz";
+
+    const options = {
+      headerOverrides: {
+        "Accept-Language": "en-us"
+      },
+      mockOption: "mockValue"
+    };
+
+    const args = { baz: "myBazValue", limit: 10, offset: 2 };
+
+    // ACT
+    const requestUrl = buildRequestUrl(baseUrl, endpointUrl, options, args);
+
+    // ASSERT
+    expect(requestUrl).toEqual(
+      "https://x.y.achievements.app/api/v1/foo/bar/myBazValue?limit=10&offset=2&mockOption=mockValue"
+    );
+  });
+});
--- src/buildRequestUrl.ts
+++ src/buildRequestUrl.ts
@@ -6,7 +6,7 @@ export const buildRequestUrl = (
   baseUrl: string,
   endpointUrl: string,
   options: Partial<AllCallOptions> = {},
-  args: Record<string, string> = {}
+  args: Record<string, string | number> = {}
 ) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars -- This is an intentional pick.
   const { headerOverrides, ...pickedOptions } = options;
