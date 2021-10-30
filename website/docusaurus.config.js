55988769e572d09bae2701e62905cf5902760fad
Wes Copeland <wlcopeland1@gmail.com>
2021-10-30 14:51:36 -0400

    docs: finish docusaurus conversion

---
+++ website/docusaurus.config.js
@@ -0,0 +1,89 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "psn-api",
+  tagline: "Dinosaurs are cool",
+  url: "https://psn-api.achievements.app",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "achievements.app", // Usually your GitHub org/user name.
+  projectName: "psn-api", // Usually your repo name.
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/achievements-app/website/edit/main/website/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "psn-api",
+
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Quick start"
+          },
+          {
+            href: "https://github.com/achievements-app/psn-api",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Twitter",
+                href: "https://twitter.com/wescopeland_"
+              }
+            ]
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/achievements-app/psn-api"
+              }
+            ]
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} achievements.app.`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme
+      }
+    })
+};
+
+module.exports = config;
