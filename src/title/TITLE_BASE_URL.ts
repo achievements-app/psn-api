2f5ab810500227ad6985368f01a160711be4f9ef
Wes Copeland <wlcopeland1@gmail.com>
2023-02-28 18:48:54 -0500

    chore: delete dead code

--- src/title/TITLE_BASE_URL.ts
+++
@@ -1,2 +0,0 @@
-export const TITLE_BASE_URL =
-  "https://m.np.playstation.com/api/catalog/v2/titles";
--- src/title/getTitle.ts
+++
@@ -1,8 +0,0 @@
-import type {
-  AllCallOptions,
-  AuthorizationPayload,
-  TitleTrophiesResponse
-} from "../models";
-import { buildRequestUrl } from "../utils/buildRequestUrl";
-import { call } from "../utils/call";
-import { TITLE_BASE_URL } from "./TITLE_BASE_URL";
