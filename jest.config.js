d8e7210a7bb321a93810cd16ddb5eb1b3dbcf663
Wes Copeland <wlcopeland1@gmail.com>
2025-05-10 19:20:48 -0400

    chore: drop codeclimate

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       coverageThreshold: {
         global: {
-          branches: 75,
+          branches: 100,
           functions: 100,
           lines: 100,
           statements: 100
