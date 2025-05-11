37a83ac683824308a37d83b0a3ae23774b7ed679
Dev-R <Rabimhd@icloud.com>
2025-05-12 02:29:10 +0300

    docs: add examples to country code and name properties

--- src/models/user-region-info.model.ts
+++ src/models/user-region-info.model.ts
@@ -4,11 +4,14 @@
 export interface UserRegionInfo {
   /**
    * The two-letter country code (ISO 3166-1 alpha-2)
+   * @example "US"
    */
   code: RegionCode;
 
   /**
    * The full name of the country in English
+   *
+   * @example "United States"
    */
   name?: string;
 }
