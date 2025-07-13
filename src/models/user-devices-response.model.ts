7b03d09a39f92a43140f6f96df7e863cad8626cf
Dev-R <Rabimhd@icloud.com>
2025-07-13 23:37:45 +0300

    fix(docs): update comments for clarity and consistency

--- src/models/user-devices-response.model.ts
+++ src/models/user-devices-response.model.ts
@@ -1,22 +1,28 @@
 export interface AccountDevicesResponse {
-  /** The User's account ID */
+  /** The user's account ID. */
   accountId: string;
 
-  /** List of devices the account is logged into */
+  /** List of devices the account is logged into. */
   accountDevices: Array<{
     /** The device ID */
     deviceId: string;
 
-    /** The device type/platform (e.g., PS5, PS4, PS3, PSVita) */
+    /**
+     * The device type/platform.
+     * @example "PS5" | "PS4" | "PS3" | "PSVita"
+     */
     deviceType: string;
 
-    /** The activation type (e.g., PRIMARY, PSN_GAME_V3) */
+    /** 
+     * The activation type.
+     * @example "PRIMARY" | "PSN_GAME_V3"
+     */
     activationType: string;
 
-    /** The device activation date in ISO format */
+    /** The device activation date in ISO format. */
     activationDate: string;
 
-    /** The account device vector */
+    /** The account device vector. */
     accountDeviceVector: string;
   }>;
 }
