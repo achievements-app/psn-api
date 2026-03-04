e7dc4de4ae041c9e810d4d190aa5d86e79fda66c
Wes Copeland <wlcopeland1@gmail.com>
2026-03-04 08:31:33 -0500

    chore(deps): fix a bunch of dependabot alerts

--- src/test/generators/trophy-counts.generator.ts
+++ src/test/generators/trophy-counts.generator.ts
@@ -6,10 +6,10 @@ export const generateTrophyCounts = (
   trophyCountsProps?: Partial<TrophyCounts>
 ): TrophyCounts => {
   return {
-    bronze: faker.datatype.number({ min: 0, max: 1000 }),
-    silver: faker.datatype.number({ min: 0, max: 1000 }),
-    gold: faker.datatype.number({ min: 0, max: 1000 }),
-    platinum: faker.datatype.number({ min: 0, max: 1 }) as 0 | 1,
+    bronze: faker.number.int({ min: 0, max: 1000 }),
+    silver: faker.number.int({ min: 0, max: 1000 }),
+    gold: faker.number.int({ min: 0, max: 1000 }),
+    platinum: faker.number.int({ min: 0, max: 1 }) as 0 | 1,
     ...trophyCountsProps
   };
 };
